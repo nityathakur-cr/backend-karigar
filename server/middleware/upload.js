8f40398cc5365a08d6984bc492687c79d7070fd6
Manrit Kaur <manrit.cr26@gmail.com>
2026-05-15 17:17:01 +0530

    Updated Upload.js

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,2 +1,45 @@
-const multer = require("multer")
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
 
+  const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      let folderName = "others";
+      if (file.fieldname === "profileImg") {
+        folderName = "profile-images";
+      }
+      const uploadPath = `uploads/${folderName}`;
+      fs.mkdirSync(uploadPath, { recursive: true });
+
+      cb(null, uploadPath);
+    },
+    filename: function (req, file, cb) {
+      const uniqueName =
+        file.fieldname + "_" + Date.now() + path.extname(file.originalname);
+
+      cb(null, uniqueName);
+    },
+  });
+
+  const allowedExt = [".jpg", ".jpeg", ".png", ".webp"];
+
+  const fileFilter = (req, file, cb) => {
+    const ext = path.extname(file.originalname).toLowerCase();
+
+    if (file.mimetype.startsWith("image") && allowedExt.includes(ext)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only image files can be uploaded"), false);
+    }
+  };
+
+  const upload = multer({
+    storage,
+    fileFilter,
+    limits: {
+      fileSize: 2 * 1024 * 1024,
+    },
+  });
+
+
+module.exports = upload;
